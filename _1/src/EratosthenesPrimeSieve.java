ee2a4432567b5c69a418133e0a23a11ccb653007
hydrogen07 <klaffenboeckv@gmx.at>
2023-09-22 12:33:58 +0200

    third commit

--- _1/src/EratosthenesPrimeSieve.java
+++ _1/src/EratosthenesPrimeSieve.java
@@ -1,6 +1,33 @@
-public class EratosthenesPrimeSieve {
+import java.util.ArrayList;
 
-    public EratosthenesPrimeSieve() {
-        
+public class EratosthenesPrimeSieve implements PrimeSieve {
+
+    private ArrayList<Integer> numbers;
+    private ArrayList<Integer> primes;
+
+    public EratosthenesPrimeSieve(int upperBound) {
+        numbers = new ArrayList<>();
+        primes = new ArrayList<>();
+        for (int i = 2; i <= upperBound; i++)
+            numbers.add(i);
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+
+    }
+
+    private void sieveAlgorithm() {
+        for (int i = 0; i < numbers.size(); i++) {
+            for (int j : numbers) {
+                if ()
+                primes.add(j);
+            }
+        }
     }
 }
