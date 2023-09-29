b4413933386a8f0fe410aa74c8b06f00525c2e14
hydrogen07 <klaffenboeckv@gmx.at>
2023-09-29 09:37:42 +0200

    testing different lambdas

--- _2/src/HalloJavaMitForEach.java
+++ _2/src/HalloJavaMitForEach.java
@@ -8,6 +8,11 @@ public class HalloJavaMitForEach {
         for (int i = 0; i < 26; i++)
             list.add(String.valueOf((char)(97+i)));
         for (String cList : list)
-            System.out.println(cList);
+            System.out.print(cList + " ");
+        System.out.println();
+        list.forEach((String s) -> System.out.print(s + " "));
+        System.out.println();
+        list.forEach(System.out::print);
+
     }
 }
