12c3519401b4d0718f4c9f8a9b508924d8dd0b4e
hydrogen07 <klaffenboeckv@gmx.at>
2023-10-05 19:54:52 +0200

    finished the other two classes

---
+++ _2/src/beispiel4/ComplexCalculator.java
@@ -0,0 +1,50 @@
+package beispiel4;
+
+public class ComplexCalculator extends AbstractCalculator {
+
+    public ComplexCalculator() {
+        super((x, y) -> {
+                    Number n = new Number();
+                    n.setA(x.getA()+y.getA());
+                    n.setB(x.getB()+y.getB());
+                    return n;},
+
+                (x, y) -> {
+                    Number n = new Number();
+                    n.setA(x.getA()-y.getA());
+                    n.setB(x.getB()-y.getB());
+                    return n;},
+
+                (x, y) -> {
+                    Number n = new Number();
+                    n.setA(x.getA()*y.getB()-x.getB()*y.getA());
+                    n.setB(0);
+                    return n;},
+
+                (x, y) -> {
+                    Number n = new Number();
+                    n.setA(x.getA()*y.getA()+x.getB()*y.getB());
+                    n.setB(0);
+                    return n;});
+    }
+
+    @Override
+    public Number add(Number x, Number y) {
+        return super.add.calc(x, y);
+    }
+
+    @Override
+    public Number subtract(Number x, Number y) {
+        return super.subtract.calc(x, y);
+    }
+
+    @Override
+    public Number multiply(Number x, Number y) {
+        return super.multiply.calc(x, y);
+    }
+
+    @Override
+    public Number divide(Number x, Number y) {
+        return super.divide.calc(x, y);
+    }
+}
